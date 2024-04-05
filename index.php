5a545def985406c5726fd89b144d694b1922822f
Cyatria Wichaksana Ristanto <130017449+adit-wr@users.noreply.github.com>
2024-04-05 14:19:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="https://img.freepik.com/premium-vector/partai-pdi_588787-87.jpg?w=996">
+    <title>PSI</title>
+</head>
+<body>
+    <div class="container">
+        <nav class="header">
+            <div class="head1">
+                <p>e-Library</p>
+            </div>
+            <div class="head2">
+                <div class="search">
+                    <input type="text" class="search1" placeholder="Search">
+                    <a class="btnsrch" href=""><span class="material-symbols-outlined">search</span></a>
+                </div>
+            </div>
+        </nav>
+        <div class="container1">
+            <div class="main"> 
+                <h1>Librarian</h1>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Phone</th>
+                        <th>Name</th>
+                        <th>
+                            <div class="tdbtn">
+                                <a class="tadd" href=""><span class="material-symbols-outlined">library_add</span>New Librarian</a>
+                            </div>
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>0888 9248 6271</td>
+                        <td class="tdname">Cyatria Wichaksana Ristanto</td>
+                        <td class="tdbtn">
+                            <a class="tview" href="">View<span class="material-symbols-outlined">visibility</span></a>
+                            <a class="tupd" href="">Update<span class="material-symbols-outlined">edit</span></a>
+                            <a class="tdlt" href="">Delete<span class="material-symbols-outlined">delete</span></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>0812 9263 8173</td>
+                        <td class="tdname">Intan Lailatul</td>
+                        <td class="tdbtn">
+                            <a class="tview" href="">View<span class="material-symbols-outlined">visibility</span></a>
+                            <a class="tupd" href="">Update<span class="material-symbols-outlined">edit</span></a>
+                            <a class="tdlt" href="">Delete<span class="material-symbols-outlined">delete</span></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>0857 2617 8219</td>
+                        <td class="tdname">Maul Seleb</td>
+                        <td class="tdbtn">
+                            <a class="tview" href="">View<span class="material-symbols-outlined">visibility</span></a>
+                            <a class="tupd" href="">Update<span class="material-symbols-outlined">edit</span></a>
+                            <a class="tdlt" href="">Delete<span class="material-symbols-outlined">delete</span></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>0881 7281 6252</td>
+                        <td class="tdname">Ali Imut</td>
+                        <td class="tdbtn">
+                            <a class="tview" href="">View<span class="material-symbols-outlined">visibility</span></a>
+                            <a class="tupd" href="">Update<span class="material-symbols-outlined">edit</span></a>
+                            <a class="tdlt" href="">Delete<span class="material-symbols-outlined">delete</span></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>0856 1729 6472</td>
+                        <td class="tdname">Akbar Gendeng</td>
+                        <td class="tdbtn">
+                            <a class="tview" href="">View<span class="material-symbols-outlined">visibility</span></a>
+                            <a class="tupd" href="">Update<span class="material-symbols-outlined">edit</span></a>
+                            <a class="tdlt" href="">Delete<span class="material-symbols-outlined">delete</span></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>0877 6251 7282</td>
+                        <td class="tdname">Yuanita Wantot</td>
+                        <td class="tdbtn">
+                            <a class="tview" href="">View<span class="material-symbols-outlined">visibility</span></a>
+                            <a class="tupd" href="">Update<span class="material-symbols-outlined">edit</span></a>
+                            <a class="tdlt" href="">Delete<span class="material-symbols-outlined">delete</span></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>0812 3243 9102</td>
+                        <td class="tdname">Rayhan Pendek</td>
+                        <td class="tdbtn">
+                            <a class="tview" href="">View<span class="material-symbols-outlined">visibility</span></a>
+                            <a class="tupd" href="">Update<span class="material-symbols-outlined">edit</span></a>
+                            <a class="tdlt" href="">Delete<span class="material-symbols-outlined">delete</span></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>0856 7829 2510</td>
+                        <td class="tdname">Dipoo</td>
+                        <td class="tdbtn">
+                            <a class="tview" href="">View<span class="material-symbols-outlined">visibility</span></a>
+                            <a class="tupd" href="">Update<span class="material-symbols-outlined">edit</span></a>
+                            <a class="tdlt" href="">Delete<span class="material-symbols-outlined">delete</span></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>9</td>
+                        <td>0865 2612 8819</td>
+                        <td class="tdname">Ammar Meduro</td>
+                        <td class="tdbtn">
+                            <a class="tview" href="">View<span class="material-symbols-outlined">visibility</span></a>
+                            <a class="tupd" href="">Update<span class="material-symbols-outlined">edit</span></a>
+                            <a class="tdlt" href="">Delete<span class="material-symbols-outlined">delete</span></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>0826 2617 9493</td>
+                        <td class="tdname">Jack Mendo</td>
+                        <td class="tdbtn">
+                            <a class="tview" href="">View<span class="material-symbols-outlined">visibility</span></a>
+                            <a class="tupd" href="">Update<span class="material-symbols-outlined">edit</span></a>
+                            <a class="tdlt" href="">Delete<span class="material-symbols-outlined">delete</span></a>
+                        </td>
+                    </tr>
+                </table>
+                <div class="cpr">
+                    <p><i class="fa fa-instagram" style="font-size:36px"></i> Copyright @aditwchksr :v</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="kanan">
+        <div class="navbar">
+            <a class="btnnav" href="">Dashboard<span class="material-symbols-outlined">home</span></a>
+            <a class="btnnav" href="">Order<span class="material-symbols-outlined">shopping_cart</span></a>
+            <a class="btnnav" href="">Librarian<span class="material-symbols-outlined">person</span></a>
+            <a class="btnnav" href="">History<span class="material-symbols-outlined">history</span></a>
+            <a class="btnnavlo" href="signin.php">Logout<span class="material-symbols-outlined">logout</span></a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ signin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="https://img.freepik.com/premium-vector/partai-pdi_588787-87.jpg?w=996">
+    <title>PSI</title>
+</head>
+<body>
+    <div class="container">
+        <div class="main">
+            <form action="">    
+                <h1>Welcome!</h1>
+                <div class="forminput">
+                    <label for="name">Username</label>
+                    <input type="text" id="name" placeholder="Enter your username" autocomplete="off">
+                </div>
+                <div class="forminput">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" placeholder="Enter your password" autocomplete="off">
+                </div>
+                <a class="btnsi" href="index.php">SIGN IN</a>
+                <div class="a">
+                    <ul>Dont have an account? <a href="signup.php">Sign Up</a></ul>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
